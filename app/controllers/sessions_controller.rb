8ea753fa1a2c17dd46e0496fddb79a8ddc6f3db5
Zack Forbing <zackforbing@gmail.com>
2016-08-30 09:12:19 -0600

    implement oauth, basic bootstrap. flashes failing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
+
     def create
-      render text: request.env["omniauth.auth"].inspect
-      # auth = request.env["omniauth.auth"]
-      # user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || User.create_with_omniauth(auth)
-      # session[:user_id] = user.id
-      # redirect_to root_url, :notice => "Signed in!"
+      auth = request.env["omniauth.auth"]
+      user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || User.create_with_omniauth(auth)
+      session[:user_id] = user.id
+      redirect_to root_url, flash[:success] = "Successfully signed in!"
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    session.clear
+    redirect_to root_url #flash[:success] = "Successfully signed out!"
   end
 end
